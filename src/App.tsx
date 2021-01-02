0afe36a1c9926c13574424c65080c3b107ce40b8
Michael <micjanic20@gmail.com>
2021-01-02 00:56:57 -0600

    simulation for first 4 steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import produce from 'immer';
 
 const numRows = 50
 const numCols = 50
+
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 1]
+]
 
 const App: React.FC = () => {
   const [grid, setGrid] = useState(() => {
@@ -14,33 +25,72 @@ const App: React.FC = () => {
     return rows
   })
 
+  const [running, setRunning] = useState(false)
+
+  const runningRef = useRef(running)
+  runningRef.current = running
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return
+    }
+
+    setGrid((g) => {
+      return produce(g, gridCopy => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK]
+              }
+            })
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1
+            }
+          }
+        }
+      })
+    })
+
+    setTimeout(runSimulation, 1000)
+  }, [])
+
   return (
-    <div
-      style={{
-        display: 'grid',
-        gridTemplateColumns: `repeat(${numCols}, 20px)`,
-      }}
-    >
-      {grid.map((rows, i) =>
-        rows.map((col, k) => (
-          <div
-            onClick={() => {
-              const newGrid = produce(grid, gridCopy => {
-                gridCopy[i][k] = 1
-              })
-              setGrid(newGrid)
-            }}
-            key={`${i}-${k}`}
-            style={{
-              width: 20,
-              height: 20,
-              backgroundColor: grid[i][k] ? 'pink' : undefined,
-              border: 'solid 1px black',
-            }}
-          ></div>
-        ))
-      )}
-    </div>
+    <>
+      <button onClick={() => setRunning(!running)}>{running ? 'stop' : 'start'}</button>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${numCols}, 20px)`,
+        }}
+      >
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <div
+              onClick={() => {
+                const newGrid = produce(grid, gridCopy => {
+                  gridCopy[i][k] = gridCopy[i][k] ? 0 : 1
+                })
+                setGrid(newGrid)
+              }}
+              key={`${i}-${k}`}
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: grid[i][k] ? 'pink' : undefined,
+                border: 'solid 1px black',
+              }}
+            ></div>
+          ))
+        )}
+      </div>
+    </>
   )
 }
 
